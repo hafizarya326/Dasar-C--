be00c928ac324561aa21edbc66bb462812c6655a
Hafiz Pratama <hafizpratama326@gmail.com>
2026-09-13 11:46:21 +0700

    first commit

---
+++ dasar2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string nama = "Hafiz";
+    int umur = 17;
+    double tinggi = 170.5;
+    char grade = 'A';
+    bool sudahLogin = true;
+
+    cout << "Nama: " << nama << endl;
+    cout << "Umur: " << umur << endl;
+    cout << "Tinggi: " << tinggi << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Login: " << sudahLogin << endl;
+
+    return 0;
+}
